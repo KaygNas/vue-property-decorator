bd3c6104d61a61f9cea14a798e4f3e51cbb4f01d
huangchengjie <597857074@qq.com>
2022-11-17 12:38:10 +0800

    fix: force ref computed compute everytime when updated to make it reactive;

--- src/decorators/Ref.ts
+++ src/decorators/Ref.ts
@@ -6,10 +6,38 @@ import { createDecorator, Vue, VueDecorator } from 'vue-class-component'
  */
 export function Ref(refKey?: string): VueDecorator {
   return createDecorator((componentOptions, key) => {
+    // some hacky trick to force componentOptions.computed[key]
+    // compute every time component updated
+    const refSymbol = {
+      symbol: Symbol(refKey || key),
+      get: (vm: any) => vm[refSymbol.symbol],
+      set: (vm: any) => vm[refSymbol.symbol] = Symbol(refKey || key)
+    }
+
+    const createData = componentOptions.data ?? (() => ({}))
+    componentOptions.data = function () {
+      const data = createData.call(this, this)
+      refSymbol.set(data)
+      return data
+    }
+
+    const created = componentOptions.created
+    componentOptions.created = function () {
+      created?.call(this)
+      refSymbol.get(this)
+    }
+
+    const beforeUpdate = componentOptions.beforeUpdate
+    componentOptions.beforeUpdate = function () {
+      beforeUpdate?.call(this)
+      refSymbol.set(this)
+    }
+
     componentOptions.computed ||= Object.create(null)
     componentOptions.computed[key] = {
       cache: false,
       get(this: Vue) {
+        refSymbol.get(this)
         return this.$refs[refKey || key]
       },
     }
--- tests/decorators/Ref.spec.ts
+++ tests/decorators/Ref.spec.ts
@@ -1,4 +1,4 @@
-import { mount, VueWrapper } from '@vue/test-utils'
+import { mount, VueWrapper, flushPromises } from '@vue/test-utils'
 import { h } from 'vue'
 import { Vue } from 'vue-class-component'
 import { Ref } from '../../src/decorators/Ref'
@@ -56,4 +56,54 @@ describe(Ref, () => {
       `)
     })
   })
+
+  describe('reactive ref when conditional rendering', () => {
+    const KEY = 'KEY'
+
+    class MyComponent extends Vue {
+      @Ref() [KEY]!: HTMLDivElement
+      visible = false
+
+
+      async mounted() {
+        await waitUntil(100)
+        this.visible = true
+      }
+
+
+      render() {
+        const { visible } = this
+        return visible ? h('div', { ref: KEY }, 'THIS-IS-MY-REF-OBJECT') : undefined
+      }
+    }
+
+    let wrapper: VueWrapper<MyComponent>
+
+    beforeEach(() => {
+      wrapper = mount(MyComponent)
+    })
+
+    it('returns ref object reactivly', async () => {
+      expect(wrapper.vm.visible).toBe(false)
+      expect(wrapper.vm[KEY]).toBeUndefined()
+
+      await waitUntil(100)
+      await flushPromises()
+
+      expect(wrapper.vm.visible).toBe(true)
+      expect(wrapper.vm[KEY]).toMatchInlineSnapshot(`
+      <div>
+        THIS-IS-MY-REF-OBJECT
+      </div>
+    `)
+
+    })
+  })
 })
+
+
+function waitUntil(time: number) {
+  return new Promise((resovle, reject) => {
+    setTimeout(resovle, time)
+  })
+}
